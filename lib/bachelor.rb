7cad2331a092199d7a16cef0f935e4643870050b
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 04:42:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,6 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.[season].each do |actor|
     if actor["status"] == "Winner"
       return actor["name"]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
